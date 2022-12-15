2d2edc8bea5f9d9ceabccc1e305c38d18b20f7e9
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-12-14 22:04:28 -0800

    setup server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(routes);
 
-app.use(require('./routes'));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+	});
+});
