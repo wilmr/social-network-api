92d105304d57cef4c550ef8d97986fe7fb359a18
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-12-14 23:38:12 -0800

    connected to mongo and finished connection

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+module.exports = mongoose.connection;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -17,6 +17,8 @@ router
 	.put(updateThought)
 	.delete(deleteThought);
 
+router.route('/:thoughtId/reactions').post(addReaction);
+
 router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
 
 module.exports = router;
